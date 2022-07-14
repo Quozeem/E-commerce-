85884b7632f98d1c98411ae629ae451786e27b23
Ademola Quozeem <72305068+Quozeem@users.noreply.github.com>
2022-07-14 12:26:26 +0100

    Add files via upload

---
+++ resources/views/email/newcustomer.blade.php
@@ -0,0 +1,121 @@
+
+<!DOCTYPE html>
+<html>
+<!---<html class="supports-js supports-no-touch supports-csstransforms supports-csstransforms3d supports-fontface"--->
+
+<!-- Mirrored from demo.tadathemes.com/HTML_Homemarket/demo/ by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 03 May 2021 21:08:27 GMT -->
+<head>
+
+	<meta charset="utf-8">
+	<title>qoztore - Online Store</title>
+	<!-- Font ================================================== -->
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700"> 
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Lato:300,400,500,600,700">
+	<!-- Font ================================================== -->
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700"> 
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Lato:300,400,500,600,700">
+	<!-- Helpers ================================================== -->
+	
+<script data-ad-client="ca-pub-1524909513204551" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<meta property="og:title" content="qoztore - Online Store">
+	<link rel="shortcut icon" type="image/x-icon" href="img/logo/good-removebg-preview.png">
+	<!---<meta property="og:image" content="../../assets/images/logo.html">
+	<meta property="og:image:secure_url" content="../../assets/images/logo.html"--->
+	<meta property="og:url" content="#">
+	<!---<meta property="og:site_name" content="Home Market Red"---->
+	
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	  <meta name="csrf-token" content="{{csrf_token()}}"/>  
+	<!-- CSS ================================================== -->
+	<link href="assets/css/jquery.fancybox.css" rel="stylesheet" type="text/css" media="all">
+	<script src="js/vendor/modernizr-2.8.3.min.js"></script>  
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" ></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script---->
+        <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css')}}">   <link rel="stylesheet" href="css/animate.min.css">
+        <link rel="stylesheet" href="{{ asset('css/magnific-popup.css')}}">
+        <link rel="stylesheet" href="{{ asset('css/fontawesome-all.min.css')}}">
+        <link rel="stylesheet" href="{{ asset('css/aos.css')}}">
+        <link rel="stylesheet" href="{{ asset('css/nice-select.css')}}">
+        <link rel="stylesheet" href="{{ asset('css/flaticon.css')}}">
+        <link rel="stylesheet" href="{{ asset('css/meanmenu.css')}}">
+        <link rel="stylesheet" href="{{ asset('css/slick.css')}}">
+        <link rel="stylesheet" href="{{ asset('css/default.css')}}">
+          <link rel="stylesheet" href="{{ asset('css/rest.css')}}">
+        <link rel="stylesheet" href="{{ ('css/style.css')}}">
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <!---<link rel="stylesheet" href="style1.css"---->
+        <link rel="stylesheet" href="{{ asset('css/responsive.css')}}">
+	<link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css')}}">
+	<link href="{{ asset('assets/css/animate.min.css')}}" rel="stylesheet" type="text/css" media="all">
+	<link href="{{ asset('assets/css/swatch.css')}}" rel="stylesheet" type="text/css" media="all">
+	<link href="{{ asset('assets/css/owl.carousel.min.css')}}" rel="stylesheet" type="text/css" media="all">
+	<link href="{{ asset('assets/css/flexslider.css')}}" rel="stylesheet" type="text/css" media="all">
+	<link href="{{ asset('assets/css/timber.scss.css')}}" rel="stylesheet" type="text/css" media="all">
+	<link href="{{ asset('assets/css/home_market.global.scss.css')}}" rel="stylesheet" type="text/css" media="all">
+	<link href="{{ asset('assets/css/home_market.style.scss.css')}}" rel="stylesheet" type="text/css" media="all">
+	<link href="{{ asset('assets/css/tada.css')}}" rel="stylesheet" type="text/css" media="all">
+	<link href="{{ asset('assets/css/spr.css')}}" rel="stylesheet" type="text/css" media="all">
+	<!-- JS ================================================== -->
+	<script src="{{ asset('assets/js/jquery.min.js')}}" type="text/javascript"></script>	
+	<script src="{{ asset('assets/js/jquery.fancybox.min.js')}}" type="text/javascript"></script>
+	<script src="{{ asset('assets/js/owl.carousel.min.js')}}" type="text/javascript"></script>
+	<script src="{{ asset('assets/js/jquery.tweet.min.js')}}" type="text/javascript"></script>
+	<script src="{{ asset('assets/js/jquery.optionSelect.js')}}" type="text/javascript"></script>
+	<script src="{{ asset('assets/js/jquery.flexslider-min.js')}}" type="text/javascript"></script>
+	<script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+    	  <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+		 
+</head>
+<body id="home-market-responsive-shopify-theme" class="1 template-">
+    <div style="padding-right:5%;padding-left:5%;margin-top:5%;margin-bottom:9%">
+<div style="text-align:center;">
+<img src="assets/images/QOZTORE22.png" class="mobile-logo" alt="Logo"></div>
+<h3 style="color:777;">Dear femi</h3>
+<p style="color:#777;font-size:16px;">     Thanks for choosing Qoztore has your marketplace.your order has been received.
+Your order number 999988,.Ordered Delivery date between <?php echo date('M d,Y') ?> 
+and <?php echo date("M d, Y", strtotime("+3 day")) ; ?>.</p>
+<a href="/customerlogin" style="color:blue">LOGIN.</a>
+<p style="font-size:18px;">Thanks,</p> 
+<table>
+    <thead>
+    <tr>
+        <th>Product</th> <th>Qty</th> <th>Price</th></tr></thead>
+        <tbody>
+            <tr>
+                <td>Shoe</td><td>2</td><td>#2,000</td>
+</tr><tr> <td>Nike</td><td>2</td><td>#2,000</td></tr></tbody>
+            <tr><td></td><td>SubTotal</td><td>#8,000</td></tr>
+</table>
+</div>
+<footer class="site-footer">       
+		
+			
+        <div class="grid__item footer_copyright">
+            <div class="wrapper">
+                <div class="grid">
+                    <div class="grid__item footer-copyright one-half small--one-whole medium--one-whole small--text-center">
+                         <h6 style="color: white;width:100%">
+                        Copyright  &#169; <?php echo date('Y');?> <a class="btn-fill btn-blue sanbg"
+                                        href="https://api.whatsapp.com/send?phone=+234 703 900 1643&amp;
+                                        text=Hi there! :)" style="color: red ;">qoZTORE</i> </a>
+                               </span> | All Rights Reserved.
+                        </h6>
+                    </div><div style="margin-right:0%;margin-left:80%">
+                    <a href="https://www.facebook.com/qoztore" style="color: #949697;
+    background: darkred;font-size:25px"><i class="fab fa-facebook-f"></i></a>
+                                     <a style="color: #949697;
+    background: darkred;font-size:25px" href="https://www.twitter.com/Qoztore"><i class="fab fa-twitter"></i></a>
+                                    <a style="color: #949697;
+    background: darkred;font-size:25px" href="https://www.instagram.com/qoztore/"><i class="fab fa-instagram"></i></a>
+                </div>  </div>
+            </div>
+        </div>
+            
+    </footer>
+                      
+                           
+	
+   
+</body></html>
