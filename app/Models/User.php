dd336508f90e2e06cdc9bd2f03f6008c8f8677c1
Ademola Quozeem <72305068+Quozeem@users.noreply.github.com>
2023-12-19 09:40:47 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,6 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
