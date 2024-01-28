281ec5f3ae01cde6833262451433e8ed027236ea
Ademola Quozeem <72305068+Quozeem@users.noreply.github.com>
2024-01-28 16:23:29 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-17VS1gGiAfsm53b835k1DWhCQJ53LbE2aZ
+
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
