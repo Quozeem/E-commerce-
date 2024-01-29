603285b59fcbf61c4841595d603326442ab806d3
Ademola Quozeem <72305068+Quozeem@users.noreply.github.com>
2024-01-29 14:57:12 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,10 +37,42 @@ class User extends Authenticatable
     {
         return $this->hasMany(Coinbase::class,'user_id') ->where('status','!=','Success');
     }
-    public function ss(){
-
-return LoginModel::find(18)->coinbase;
+     public function fetch_deposits_details($data=[])
+       {
+        $response=[
+  "data"=>[
+  [
+    "id"=> "birch5mm",
+    "type"=> "coin_address",
+    "currency"=> "trx",
+    "amount"=> "1.0",
+    "txid"=> "036dc5da61f321a77e8454bdee3ahfgh5bf3fda909395bd00d6daf5950d9a5f34",
+     "status"=> "accepted",
+  ],
+   [
+    "id"=> "birch5mm",
+    "type"=> "coin_address",
+    "currency"=> "btc",
+    "amount"=> "21.0",
+    "txid"=> "036dc5da61f321a77e8454bdee3acf45bf3fda909395bd00d6daf5950d9a5f34",
+     "status"=> "accepted",
+   ]
+   ]
+  ];
+           $response=$response['data'];
+foreach($response as $res)
+{
+   $ref_txid=$res['txid'];
+    foreach($ref_txinoice as $ref_txid){
+         $data[]=   LoginModel::find($user_data->user_id)->coinbase($ref_txid);
     }
+
+}
+// }catch(\Exception $e){
+//      return 'Error Try again';
+// }
+return $data;
+       }}
     /**
      * The attributes that should be cast.
      *
