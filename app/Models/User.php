8800a5c10d2877d71c0287d77a563a3cfb620b78
Ademola Quozeem <72305068+Quozeem@users.noreply.github.com>
2024-02-08 09:37:39 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,13 +10,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-      public function marchant_address():array
-   {
-          $user_data=json_encode([
-    'quidax_user_id' => 'me']);
-          return ($user_data->quidax_user_id);
-   } Attempt to read property &quot;quidax_user_id&quot; on string in file
-    use HasApiTokens, HasFactory, Notifiable;
+       use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
