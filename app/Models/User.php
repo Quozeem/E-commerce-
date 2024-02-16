d0ef131d42288e60056c886681f8ea596cc5bbdc
Ademola Quozeem <72305068+Quozeem@users.noreply.github.com>
2024-02-16 11:05:35 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,37 +17,13 @@ class User extends Authenticatable
      *
      * @var string[]
      */
-        $Quidax=new Quidax();
-        $Quidax=  $Quidax->where('user_id',$userId)->get();
-    protected $fillable = [
-        'fname',
-        'email',
-        'password',
-    ];
-
+      
     /**
      * The attributes that should be hidden for serialization.
      *
      * @var array
      */
-       i got this error  Malformed UTF-8 characters, possibly incorrectly encoded in file modify the code below to work correctlly
-        public function wallet_qrcode($currency){
-$address_img= $this->wallet_qrcode($response);
- return ([
-     'address'=>$response->data->address,
-     'img_qrcode'=>$address_img->image,
-     'currency'=>$currency,
-     'network'=>$network
-     ]);
-   }
-        public function wallet_qrcode($response)
-   {
-       $address =$response->data->address;
-       $size=200;
-       $endpoint=config('services.QRCODE.qrcode_endpoint').'?data='.urlencode($address)."&size={$size}x{$size}";
-       $response=Http::get($endpoint);
-       return response(['image'=>$response->body(),'Content-Type'=>'image/png']);
-   }
+     
     protected $hidden = [
         'password',
         'remember_token',
