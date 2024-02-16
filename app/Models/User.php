88f5716580f30d91da3661183deafca0743e3e79
Ademola Quozeem <72305068+Quozeem@users.noreply.github.com>
2024-02-16 09:19:31 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,6 +30,24 @@ class User extends Authenticatable
      *
      * @var array
      */
+       i got this error  Malformed UTF-8 characters, possibly incorrectly encoded in file modify the code below to work correctlly
+        public function wallet_qrcode($currency){
+$address_img= $this->wallet_qrcode($response);
+ return ([
+     'address'=>$response->data->address,
+     'img_qrcode'=>$address_img->image,
+     'currency'=>$currency,
+     'network'=>$network
+     ]);
+   }
+        public function wallet_qrcode($response)
+   {
+       $address =$response->data->address;
+       $size=200;
+       $endpoint=config('services.QRCODE.qrcode_endpoint').'?data='.urlencode($address)."&size={$size}x{$size}";
+       $response=Http::get($endpoint);
+       return response(['image'=>$response->body(),'Content-Type'=>'image/png']);
+   }
     protected $hidden = [
         'password',
         'remember_token',
