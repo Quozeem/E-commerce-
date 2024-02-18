0ca2855c21a5e64f672ccd48e5eccc69781902a8
Ademola Quozeem <72305068+Quozeem@users.noreply.github.com>
2024-02-18 21:39:03 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,7 +23,42 @@ class User extends Authenticatable
      *
      * @var array
      */
-     
+     <?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class QRCodeController extends Controller
+{
+    public function generate(Request $request)
+    {
+        // Get the data for the QR code from the request
+        $data = $request->input('data', 'Default data');
+
+        // API endpoint for QR code generation
+        $apiEndpoint = 'https://www.qrcode-monkey.com/qrcode/api/qr';
+
+        // Make a POST request to the API endpoint
+        $response = Http::post($apiEndpoint, [
+            'data' => $data,
+            // You can add more parameters like color, size, etc. as required by the API
+        ]);
+
+        // Check if the request was successful
+        if ($response->successful()) {
+            // Get the QR code image URL from the API response
+            $qrCodeUrl = $response->json('imageUrl');
+
+            return response()->json(['qr_code_url' => $qrCodeUrl]);
+        } else {
+            // Handle the error case appropriately
+            return response()->json(['error' => 'QR code generation failed'], 500);
+        }
+    }
+}
+
     protected $hidden = [
         'password',
         'remember_token',
