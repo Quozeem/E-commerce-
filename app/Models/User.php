64f596d94a227962f93ceaabc21f1f7bc7f43b80
Ademola Quozeem <72305068+Quozeem@users.noreply.github.com>
2024-03-26 13:53:37 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,30 +9,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-     <select required name="sell_network" onChange={WithdrawalHandler} className="form-control" id="ngn" >
-                <option value=''>&rarr; Sell </option>
-                <option value="BTC">&rarr; BTC </option>
-                    <option value="ETH">&rarr; ETH </option>
-                    <option value="USDT">&rarr; USDT </option>
-              </select>
-       use HasApiTokens, HasFactory, Notifiable;
- const [currencyRate,setcurrencyRate]=useState()
-  const [loadcurrentRate,setloadcurrentRate]=useState(true)
-  useEffect(()=>{
-    async function CURRENCY_RATE(){
-      if(loadcurrentRate){
-  axios
-  .get(`https://coinremitter.com/api/v3/get-coin-rate`)
-      .then((res)=>{
-        if(res.data.msg == 'success'){
-            setcurrencyRate(res.data.data.`${Withdrawdata.sell_network}`.price)
-            setloadcurrentRate(false)
-        }
-        })
-      }
-      }
-      CURRENCY_RATE
-    },[])
+    
     /**
      * The attributes that are mass assignable.
      *
